20a67779094a90133e88e2c1ac1e37fd65ff15cc
sjzjams <sjzjams@gmail.com>
2018-08-27 16:59:59 +0800

    删除不需要引用的控件css

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,17 +1,21 @@
 <template>
   <footer class="ui-bottom bottom-fixed">
-      <a href="/" class="ui-bottom-item ui-bottom-item-on"> <span class="ui-bottom-icon ui-bottom-icon-shouye"></span>
+    <nuxt-link class="ui-bottom-item ui-bottom-item-on" to="/">
+      <span class="ui-bottom-icon ui-bottom-icon-shouye"></span>
         <p class="ui-bottom-tabbar">首页</p>
-      </a>
-      <a href="/bid/investlist" class="ui-bottom-item"> <span class="ui-bottom-icon ui-bottom-icon-invest"></span>
+    </nuxt-link>
+    <nuxt-link class="ui-bottom-item" to="/bid/investlist">
+      <span class="ui-bottom-icon ui-bottom-icon-invest"></span>
         <p class="ui-bottom-tabbar">项目</p>
-      </a>
-      <a href="/find" class="ui-bottom-item"> <span class="ui-bottom-icon ui-bottom-icon-find"></span>
+    </nuxt-link>
+    <nuxt-link class="ui-bottom-item" to="/find">
+      <span class="ui-bottom-icon ui-bottom-icon-find"></span>
         <p class="ui-bottom-tabbar">发现</p>
-      </a>
-      <a href="/personal" class="ui-bottom-item"> <span class="ui-bottom-icon ui-bottom-icon-my"></span>
+    </nuxt-link>
+    <nuxt-link class="ui-bottom-item" to="/personal">
+      <span class="ui-bottom-icon ui-bottom-icon-my"></span>
         <p class="ui-bottom-tabbar">我的</p>
-      </a>
+    </nuxt-link>
     </footer>
 </template>
 
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -2,29 +2,8 @@ import Vue from 'vue'
 import {
   Menu,
   MenuItem,
-  Input,
-  Button,
-  Tag,
-  Icon,
-  Badge,
-  Card,
-  Container,
-  Header,
-  Main,
-  Loading,
-  Footer
+  Loading
 } from 'element-ui'
-
 Vue.use(Menu)
 Vue.use(MenuItem)
-Vue.use(Input)
-Vue.use(Button)
-Vue.use(Tag)
-Vue.use(Icon)
-Vue.use(Badge)
-Vue.use(Card)
-Vue.use(Container)
-Vue.use(Header)
-Vue.use(Main)
 Vue.use(Loading)
-Vue.use(Footer)
